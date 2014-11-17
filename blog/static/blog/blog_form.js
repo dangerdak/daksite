4f0c60ea3cc195f6b9e916fb09dea22286bbc9bd
dangerdak <dakshina.scott@gmail.com>
2014-11-17 13:16:41 +0000

    Rudimentary pics & captions

--- blog/static/blog/blog_form.js
+++ blog/static/blog/blog_form.js
@@ -1,12 +1,18 @@
 function pasteHTML(event) {
+	var add;
 	var pos = document.getElementById("id_body").selectionStart;
 	var text = $("#id_body").val();
-	var add = event.target.outerHTML;
+	if ( $(event.target).is("figure") ) {
+		add = event.target.outerHTML;
+	}
+	else {
+			add = this.outerHTML;
+	}
 	$("#id_body").val(text.substring(0, pos) + add + text.substring(pos));
 }
 
 $(window).load(function() {
-	var images = $(".image-preview img");
+	var images = $(".image-preview");
 		$(images).each(function() {
 			$(this).on("click", pasteHTML);
 		});
